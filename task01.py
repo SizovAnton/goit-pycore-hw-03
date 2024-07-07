62a22fc88d280bd6206e4c2ef91dfae665fa8818
AntonSizov <sizov.anton.v@gmail.com>
2024-07-07 13:04:48 +0300

    task01

---
+++ task01.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+def get_days_from_today (date):
+  try:
+
+    datetime_obj = datetime.strptime(date, '%Y.%m.%d').date()
+    current_date = datetime.today().date()
+
+    interval = current_date.toordinal() - datetime_obj.toordinal()
+    # return interval
+
+    print(interval)
+
+  except:
+    print(f'Date must be yyyy.mm.dd format')
+
+  
+
+
+get_days_from_today("2024.07.14")
